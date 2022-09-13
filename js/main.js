95555c506bea02ca46420e4055768271bb5cd4cb
Steve Fallet <steve.fallet@gmail.com>
2022-09-13 11:01:18 +0200

    INF2B

--- js/main.js
+++ js/main.js
@@ -5,4 +5,16 @@
 
 'use strict';
 
-console.log('Test liaison JS');
+// A l'envoi du formulaire
+const formulaire = document.querySelector('form');
+formulaire.addEventListener('submit', function(event){
+    event.preventDefault(); // Annule l'envoi du formulaire
+    alert('Envoyé !');
+});
+
+// Au changement de la valeur de la liste
+const listeBackground = document.getElementById('background');
+listeBackground.addEventListener('change', function(){
+    const image = `./img/backgrounds/${listeBackground.value}`
+    document.body.style.backgroundImage = `url(${image})`
+});
